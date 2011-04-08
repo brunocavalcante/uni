119c55b439f50ad53dbc841a2123fa1da9da5679
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-04-07 21:52:44 -0300

    jquery multipleselect em disciplinas/editar

---
+++ public/javascripts/uni.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  initMultiSelect()
+});
+
+function initMultiSelect() {
+  $.extend($.ui.multiselect, {
+    locale: {
+      addAll:'Incluir todos',
+      removeAll:'Remover todos',
+      itemsCount:'itens selecionados'
+    }
+  });
+  
+  $('select[multiple=multiple]').multiselect({dividerLocation: 0.5})
+}
