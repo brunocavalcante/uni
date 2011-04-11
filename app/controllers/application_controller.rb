821602a9ce6d94bb703fd9ecac040dba64a151b4
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-04-11 19:53:27 -0300

    i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+  
+  def set_locale
+    # if params[:locale] is nil then I18n.default_locale will be used
+    I18n.locale = session[:locale]
+  end
+  
 end
