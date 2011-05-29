0cf16769ed407423ed168b9d602009da82d53d5a
Bruno <bruno@bruno-laptop.(none)>
2011-05-29 01:20:52 -0300

    Excluir foto + tradução PT

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,6 +9,11 @@ class UserController < ApplicationController
   
   def update
     respond_to do |format|
+      if params[:delete_photo]
+        params[:photo] = nil
+        @user.photo = nil
+      end
+      
       if @user.update_attributes(params[:user])
         format.html { redirect_to({:action => 'index'}, :notice => 'User was successfully updated.') }
         format.xml  { head :ok }
