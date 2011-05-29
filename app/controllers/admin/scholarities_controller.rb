faec2d947975f4dccabd25676a41867c44565d2b
Bruno <bruno@bruno-laptop.(none)>
2011-05-29 10:19:36 -0300

    Escolaridades + prototipo de Detalhes Disciplina

--- app/controllers/admin/scholarities_controller.rb
+++ app/controllers/admin/scholarities_controller.rb
@@ -10,46 +10,46 @@ class Admin::ScholaritiesController < ApplicationController
   end
 
   def show
-    @escolaridade = Scholarity.find(params[:id])
+    @scholarity = Scholarity.find(params[:id])
     
     respond_to do |format|
       format.html # show.html.erb
-      format.xml { render :xml => @escolaridade }
-      format.json { render :json => @escolaridade }
+      format.xml { render :xml => @scholarity }
+      format.json { render :json => @scholarity }
     end
   end
 
   def new
-    @escolaridade = Scholarity.new
+    @scholarity = Scholarity.new
   end
 
   def edit
-    @escolaridade = Scholarity.find(params[:id])
+    @scholarity = Scholarity.find(params[:id])
   end
 
   def create
-    @escolaridade = Scholarity.new(params[:escolaridade])
+    @scholarity = Scholarity.new(params[:scholarity])
 
-    if @escolaridade.save
-        redirect_to([:admin, @escolaridade], :notice => 'Scholarity was successfully created.')
+    if @scholarity.save
+        redirect_to([:admin, @scholarity], :notice => 'Scholarity was successfully created.')
     else
         render :action => "new"
     end
   end
 
   def update
-    @escolaridade = Scholarity.find(params[:id])
+    @scholarity = Scholarity.find(params[:id])
 
-    if @escolaridade.update_attributes(params[:escolaridade])
-        redirect_to([:admin, @escolaridade], :notice => 'Scholarity was successfully updated.')
+    if @scholarity.update_attributes(params[:scholarity])
+        redirect_to([:admin, @scholarity], :notice => 'Scholarity was successfully updated.')
     else
         render :action => "edit"
     end
   end
 
   def destroy
-    @escolaridade = Scholarity.find(params[:id])
-    @escolaridade.destroy
+    @scholarity = Scholarity.find(params[:id])
+    @scholarity.destroy
 
     redirect_to :action => 'index'
   end
