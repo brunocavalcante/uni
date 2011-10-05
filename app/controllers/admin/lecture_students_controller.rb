14103aa5145726c379a9bb88abb6375390ae397d
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-10-04 21:41:06 -0300

    minor improvements

--- app/controllers/admin/lecture_students_controller.rb
+++ app/controllers/admin/lecture_students_controller.rb
@@ -13,4 +13,20 @@ class Admin::LectureStudentsController < ApplicationController
   def new
     @lecture_student = LectureStudent.new
   end
+  
+  def create
+    @lecture_student = LectureStudent.new(params[:lecture_student])
+    @lecture_student.lecture = @lecture
+    @lecture_student.student = Student.find_by_code(params[:student_code])
+    
+    respond_to do |format|
+      if @lecture_student.save
+        format.html { redirect_to([:admin, @academic_period, @lecture], :notice => 'Student was successfully created.') }
+        format.xml  { render :xml => @lecture_student, :status => :created, :location => @discipline }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @lecture_student.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,9 @@ class Student < ActiveRecord::Base
   has_many :curriculum_students, :dependent => :destroy
   has_many :curriculums, :through => :curriculum_students
   
+  has_many :lecture_students, :dependent => :destroy
+  has_many :lectures, :through => :lecture_students
+  
   accepts_nested_attributes_for :person
     
   def to_param
