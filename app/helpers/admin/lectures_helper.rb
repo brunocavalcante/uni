f057461da8ab0cfb9410962e862a1579c88d9a6f
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-10-13 16:56:11 -0300

    small correction

--- app/helpers/admin/lectures_helper.rb
+++ app/helpers/admin/lectures_helper.rb
@@ -1,2 +1,11 @@
 module Admin::LecturesHelper
+  def get_day_names_map
+    day_names = {}
+    i = 0
+    for day in (t 'date.abbr_day_names')
+      day_names[day] = i
+      i += 1
+    end
+    return day_names.sort{|a,b| a[1] <=> b[1]}
+  end
 end
