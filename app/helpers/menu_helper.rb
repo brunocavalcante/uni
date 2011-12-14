c27d60101a29431e1f0bd9b1a4515cb1b5d3b14b
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-12-14 18:40:50 -0300

    HAML migration (still in progress)

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,38 +1,38 @@
 module MenuHelper
   def menu_class_start
-    return 'class=active' if ['admin', 'professor', 'student'].include?(first_url_parameter) && current_controller == nil
+    return 'active' if ['admin', 'professor', 'student'].include?(first_url_parameter) && current_controller == nil
   end
   
   def menu_class_courses
-    return 'class=active' if current_controller == 'courses'
+    return 'active' if current_controller == 'courses'
   end
   
   def menu_class_professors
-    return 'class=active' if current_controller == 'professors'
+    return 'active' if current_controller == 'professors'
   end
   
   def menu_class_students
-    return 'class=active' if current_controller == 'students'
+    return 'active' if current_controller == 'students'
   end
   
   def menu_class_academic_periods
-    return 'class=active' if current_controller == 'academic_periods'
+    return 'active' if current_controller == 'academic_periods'
   end
   
   def menu_class_parameters
-    return 'class=active' if ['parameters', 'course_categories', 'scholarities'].include? current_controller
+    return 'active' if ['parameters', 'course_categories', 'scholarities'].include? current_controller
   end
   
   def menu_class_lectures
-    return 'class=active' if current_controller == 'lectures'
+    return 'active' if current_controller == 'lectures'
   end
   
   def menu_class_registration
-    return 'class=active' if current_controller == 'registration'
+    return 'active' if current_controller == 'registration'
   end
   
   def menu_class_reports
-    return 'class=active' if current_controller == 'reports'
+    return 'active' if current_controller == 'reports'
   end
   
   def first_url_parameter
