e3c1a40f3201457a66bd0df767c499a85349753c
Bruno Cavalcante <brunofcavalcante@gmail.com>
2012-03-23 13:59:54 -0300

    minor fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,23 +4,28 @@ module ApplicationHelper
       breadcrumb = ''
       so_far = '/'
       elements = url.split('/')
-      for i in 1...elements.size
-
-        so_far += elements[i] + '/'
-
-        if elements[i] =~ /^\d+$/
-          begin
-            breadcrumb += '<li>' + link_to_if(i != elements.size - 1, eval("#{elements[i - 1].singularize.camelize}.find(#{elements[i]}).name").gsub("_"," ").to_s, so_far) + '</li>'
-          rescue
-          breadcrumb += '<li>' + elements[i] + '</li>'
+      
+      if elements.size > 3
+        for i in 1...elements.size
+  
+          so_far += elements[i] + '/'
+  
+          if elements[i] =~ /^\d+$/
+            begin
+              breadcrumb += '<li>' + link_to_if(i != elements.size - 1, eval("#{elements[i - 1].singularize.camelize}.find(#{elements[i]}).name").gsub("_"," ").to_s, so_far) + '</li>'
+            rescue
+            breadcrumb += '<li>' + elements[i] + '</li>'
+            end
+          else
+            breadcrumb += '<li>' + link_to_if(i != elements.size - 1, (I18n.t 'url.' + elements[i].gsub("-", "_").gsub(/\?.*/, '')), so_far) + '</li>'
           end
-        else
-          breadcrumb += '<li>' + link_to_if(i != elements.size - 1, (I18n.t 'url.' + elements[i].gsub("-", "_").gsub(/\?.*/, '')), so_far) + '</li>'
+  
+          breadcrumb += " <span class='divider'>/</span> " if i != elements.size - 1
         end
-
-        breadcrumb += " <span class='divider'>/</span> " if i != elements.size - 1
+        breadcrumb
+      else
+        false
       end
-      breadcrumb
     rescue
       'Not available'
     end
